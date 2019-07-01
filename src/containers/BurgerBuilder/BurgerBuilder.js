ad7e4eb669a608e3e14400a1e750e4e44051ab48
Mary Kouvela <marykouvela@gmail.com>
2019-07-01 10:49:42 +0300

    completed routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,28 +83,16 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: "Mary Kouvela",
-        //         address: {
-        //             street: "Mary's Street",
-        //             zipCode: "77777",
-        //             country: "Greece"
-        //         },
-        //         emai: "mary@mary.com"
-        //     },
-        //     deliveryMethod: "fastest"
-        // }
-
-        // axios.post("/orders.json", order).then(() => {
-        //     this.setState({loading: false, purchasing: false});
-        // }).catch(() => {
-        //     this.setState({loading: false, purchasing: false});
-        // });
-        this.props.history.push("/checkout");
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push("price=" + this.state.totalPrice);
+        const queryString = queryParams.join("&");
+        this.props.history.push({
+            pathname: "/checkout",
+            search: "?" + queryString
+        });
     }
 
     render() {
